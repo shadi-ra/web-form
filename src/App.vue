1a7e1ad0626a0e94089c0efc4a90b1b92e4ac995
shadi-ra <shadi_rahbar74@yahoo.com>
2023-08-30 14:43:22 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<signUpFrom/>
+</template>
+
+<script>
+import signUpFrom from './components/signUpForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    signUpFrom
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  margin: 0;
+  background: #eee;
+
+}
+</style>
